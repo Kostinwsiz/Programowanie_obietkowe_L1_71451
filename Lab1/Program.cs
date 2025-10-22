45fa1dd7c7c759075f98da45955d74451d34e1e9
Michal Kostin <michalkostin@gmail.com>
2025-10-22 14:46:30 +0200

    Third commit

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-string hello = "Hello world";
+﻿//zadanie 18
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    static void Main() 
+    {
+        Console.WriteLine("to jest 1 cwiczenie");
+    }
+}
+ 
+class Zwierze
+{
+    private string name = "kapibara";
+    private string variety = "gryzoń";
+    private int numberOfLegs = 4;
+    private int numOfAnimals = 0;
+}
