a1a51fc76966681333b24dff0f5b26ec3decb574
71451 <71451@RA117B-01>
2025-11-03 14:18:11 +0100

    sjkffjk

---
+++ LAB3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LAB3
+{
+    public interface IModular
+    {
+        double Module();
+    }
+
+    public sealed class ComplexNumber : ICloneable, IEquatable<ComplexNumber>, IModular
+    {
+        private double re;
+        private double im;
+
+        public double Re
+        {
+            get => re;
+            set => re = value;
+        }
+
+        public double Im
+        {
+            get => im;
+            set => im = value;
+        }
+
+   
