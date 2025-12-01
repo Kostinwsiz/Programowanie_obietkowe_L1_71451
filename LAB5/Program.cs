3b8ca275b669a8dcbf26170d0a9abae22eff2964
71451 <71451@RA117B-01>
2025-12-01 14:07:49 +0100

    Lab5

---
+++ LAB5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void ZapiszDoPliku()
+    {
+        Console.WriteLine("Podaj liczbe tekstuw, kture chcesz wprowadzić: ");
+        int liczbaTekstuw;
+
+        while (!int.TryParse(Console.ReadLine(), out liczbaTekstuw) || liczbaTekstuw <= 0)
+        {
+            Console.WriteLine("Prosze podać poprawnu liczbu większą od 0");
+        }
+
+        using (StreamWriter writer = new StreamWriter("wprowadzone_teksty.txt"))
+        {
+            for (int i = 1; i <= liczbaTekstuw; i++)
+            {
+                Console.WriteLine($"Podaj tekst nr {i}:");
+                string tekst = Console.ReadLine();
+                writer.WriteLine(tekst);
+            }
+        }
+
+        Console.WriteLine("Wszystkie wpisane informacje zostały zapisane do pliku");
+    }
+
+    static void Main(string[] args)
+    {
+        ZapiszDoPliku();
+    }
+}
---
+++ Programowanie_obietkowe_L1_71451/lab5.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programowanie_obietkowe_L1_71451
+{
+    internal class lab5
+    {
+    }
+}
